573ecc8f406bc9f0e440dc50981bb9d99f172036
chenmy1903 <chenmy10@outlook.com>
2022-03-30 09:00:27 +0800

    Enjoy~

--- fast_download.py
+++ fast_download.py
@@ -3,12 +3,16 @@ import os
 import requests
 
 def main():
-    url = input("请输入错误的URL: ")
-    join_url = "https://cdn-1258575882.cos.ap-guangzhou.myqcloud.com/gameList/wuZiQi/hhfhdf_1.0.37/web-mobile/" + url
-    file = requests.get(join_url)
-    os.makedirs(os.path.dirname(os.path.abspath(url)))
-    with open(os.path.abspath(url), "wb") as f:
-        f.write(file.content)
+    while True:
+        url = input("请输入错误的URL: ").replace("https://chenmy1903.tk/wuziqi/", "")
+        if not url:
+            break
+        join_url = "https://cdn-1258575882.cos.ap-guangzhou.myqcloud.com/gameList/wuZiQi/hhfhdf_1.0.37/web-mobile/" + url
+        file = requests.get(join_url)
+        if not os.path.isdir(os.path.dirname(os.path.abspath(url))):
+            os.makedirs(os.path.dirname(os.path.abspath(url)))
+        with open(os.path.abspath(url), "wb") as f:
+            f.write(file.content)
     print("开始提交代码")
     os.system("git pull up master")
     os.system("git add .")
